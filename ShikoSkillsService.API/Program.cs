b29d183706b4c6197c2a0b26fd8c74258580b6e1
dana <danaamirr12@gmail.com>
2026-06-01 01:35:25 +0200

    fix: cors allow any origin

--- ShikoSkillsService.API/Program.cs
+++ ShikoSkillsService.API/Program.cs
@@ -17,10 +17,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins(
-                "http://localhost:3000",
-                "https://localhost:3000"
-              )
+        policy.AllowAnyOrigin()
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
