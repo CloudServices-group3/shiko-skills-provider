f7c93197f87ebd2e52c17a726deb9a484bd34c4b
dana <danaamirr12@gmail.com>
2026-06-01 01:27:29 +0200

    fix: cors localhost

--- ShikoSkillsService.API/Program.cs
+++ ShikoSkillsService.API/Program.cs
@@ -17,7 +17,10 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.AllowAnyOrigin()
+        policy.WithOrigins(
+                "http://localhost:3000",
+                "https://localhost:3000"
+              )
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
