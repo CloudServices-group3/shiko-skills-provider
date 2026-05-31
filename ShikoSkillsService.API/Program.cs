b2d7acfff2fc487003e9ebcb9d24d9fc6b8772e8
dana <danaamirr12@gmail.com>
2026-05-31 23:59:33 +0200

    fix: uppdatera gitignore, rensa bin/obj

--- ShikoSkillsService.API/Program.cs
+++ ShikoSkillsService.API/Program.cs
@@ -49,6 +49,12 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<SkillsDbContext>();
+    await db.Database.MigrateAsync();
+}
+
 app.UseCors();
 
 app.MapOpenApi();
